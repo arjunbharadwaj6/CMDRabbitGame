7b3cd2611fb574fd60084a9e3c80b01625336a4c
arjunbharadwaj6 <166818310+arjunbharadwaj6@users.noreply.github.com>
2024-04-12 22:23:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -32,8 +32,12 @@ def print_grid(g):
 
 
 # printing function to avoid repetition
-def print_oob():
-    print("You can't jump as you will go outside the board")
+def print_cj():
+    print("You can't jump given the circumstance.")
+
+
+def print_nath():
+    print("Since you are not adjacent to a hole you can't place the carrot.")
 
 
 def grid_generator(grid_size, n_carrots, n_holes):
@@ -53,12 +57,12 @@ def grid_generator(grid_size, n_carrots, n_holes):
     # Check if the entered values match the guidlines
     while True:
         # If the user entered 0 carrots
-        if n_carrots == 0:
-            print("Please enter 1 or more carrots or the game can never end")
+        if n_carrots < 2:
+            print("Please enter 2 or more carrots or the game can never end")
             n_carrots = int(input("Enter number of carrots: "))
         # If the user entered 0 holes
-        elif n_holes == 0:
-            print("Please enter 1 or more holes or the game can never end")
+        elif n_holes < 2:
+            print("Please enter 2 or more holes or the game can never end")
             n_holes = int(input("Enter number of holes: "))
         # If It matches the guidlines
         elif n_carrots + n_holes < grid_size * grid_size and grid_size >= 10:
@@ -147,7 +151,7 @@ grid, x_r, y_r = grid_generator(
 )  # Calls the grid_generator function and gets all the required values
 
 picked = False  # This is a boolean that is false if the rabbit has not picked a carrot and true if the rabbit has picked the carrot
-
+game_over = False
 
 print_grid(grid)
 print("Use arrow keys or wasd to move")
@@ -156,136 +160,178 @@ print(
     "You can only pick up one carrot. You can place the picked up carrot in a hole by using the key 'p'."
 )
 
-while True:
 
-    def movement(event):
-        """
-        This function defines the movement of the rabbit
+def movement(event):
+    """
+    This function defines the movement of the rabbit
 
-        Args:
-            event (string): The key which is pressed
-        """
-        global x_r, y_r, grid_size, picked  # These variable are global so that i can access their value inside the function
+    Args:
+        event (string): The key which is pressed
+    """
+    global x_r, y_r, grid_size, game_over, picked  # These variable are global so that i can access their value inside the function
 
-        # Clears the screen
-        clear_screen()
+    # Clears the screen
+    clear_screen()
 
-        # If the up or the w key has been pressed
-        if event.name == "up" or event.name == "w":
-            if y_r == 0:  # edge of the board
-                print("You can't go up. You are at the edge of the board.")
-            elif grid[y_r - 1][x_r] == "c":  # there is a carrot above
-                print("You can't go up as there is a carrot.")
-            elif grid[y_r - 1][x_r] == "O":  # there is a hole above
-                print("You can't go up as there is a hole. Use 'J' to jump.")
-            else:  # if it is none of them that means that the rabbit can go up
-                # rabbits current position becomes a path way stone
-                grid[y_r][x_r] = "-"
-                y_r -= 1  # rabbits y cord decreases by 1
+    # If the up or the w key has been pressed
+    if event.name == "up" or event.name == "w":
+        if y_r == 0:  # edge of the board
+            print("You can't go up. You are at the edge of the board.")
+        elif grid[y_r - 1][x_r] == "c":  # there is a carrot above
+            print("You can't go up as there is a carrot.")
+        elif grid[y_r - 1][x_r] == "O":  # there is a hole above
+            print("You can't go up as there is a hole. Use 'J' to jump.")
+        else:  # if it is none of them that means that the rabbit can go up
+            # rabbits current position becomes a path way stone
+            grid[y_r][x_r] = "-"
+            y_r -= 1  # rabbits y cord decreases by 1
 
-        # Works in the same way as when the up or w key is pressed
-        elif event.name == "down" or event.name == "s":
-            if y_r == grid_size - 1:
-                print("You can't go down. You are at the edge of the board.")
+    # Works in the same way as when the up or w key is pressed
+    elif event.name == "down" or event.name == "s":
+        if y_r == grid_size - 1:
+            print("You can't go down. You are at the edge of the board.")
+        elif grid[y_r + 1][x_r] == "c":
+            print("You can't go down as there is a carrot.")
+        elif grid[y_r + 1][x_r] == "O":
+            print("You can't go down as there is a hole. Use 'J' to jump.")
+        else:
+            grid[y_r][x_r] = "-"
+            y_r += 1
+
+    # Works in the same way as when the up or w key is pressed
+    elif event.name == "left" or event.name == "a":
+        if x_r == 0:
+            print("You can't go left. You are at the edge of the board.")
+        elif grid[y_r][x_r - 1] == "c":
+            print("You can't go left as there is a carrot.")
+        elif grid[y_r][x_r - 1] == "O":
+            print("You can't go left as there is a hole. Use 'J' to jump.")
+        else:
+            grid[y_r][x_r] = "-"
+            x_r -= 1
+
+    # Works in the same way as when the up or w key is pressed
+    elif event.name == "right" or event.name == "d":
+        if x_r == grid_size - 1:
+            print("You can't go right. You are at the edge of the board.")
+        elif grid[y_r][x_r + 1] == "c":
+            print("You can't go right as there is a carrot.")
+        elif grid[y_r][x_r + 1] == "O":
+            print("You can't go right as there is a hole. Use 'J' to jump.")
+        else:
+            grid[y_r][x_r] = "-"
+            x_r += 1
+
+    # If the j key is pressed the rabbit has to jump
+    elif event.name == "j":
+        # Checking is it able to jump up
+        if (
+            y_r > 1
+            and grid[y_r - 1][x_r] == "O"
+            and (grid[y_r - 2][x_r] != "O" and grid[y_r - 2][x_r] != "c")
+        ):
+            grid[y_r][x_r] = "-"
+            y_r -= 2
+        # Checking is it able to jump down
+        elif (
+            y_r < grid_size - 2
+            and grid[y_r + 1][x_r] == "O"
+            and (grid[y_r + 2][x_r] != "O" and grid[y_r + 2][x_r] != "c")
+        ):
+            grid[y_r][x_r] = "-"
+            y_r += 2
+        # Checking is it able to jump to the left
+        elif (
+            x_r > 1
+            and grid[y_r][x_r - 1] == "O"
+            and (grid[y_r][x_r - 2] != "O" and grid[y_r][x_r - 2] != "c")
+        ):
+            grid[y_r][x_r] = "-"
+            x_r -= 2
+        # Checking is it able to jump to the right
+        elif (
+            x_r < grid_size - 2
+            and grid[y_r][x_r + 1] == "O"
+            and (grid[y_r][x_r + 2] != "O" and grid[y_r][x_r + 2] != "c")
+        ):
+            grid[y_r][x_r] = "-"
+            x_r += 2
+
+    # If the p key is pressed it has to either pick up a carrot or place it
+    elif event.name == "p":
+        # If the rabbit has not picked up the carrot
+        if not picked:
+            # If there is a carrot above the rabbit
+            if grid[y_r - 1][x_r] == "c":
+                grid[y_r - 1][x_r] = "-"
+                picked = True
+            # If there is a carrot below the rabbit
             elif grid[y_r + 1][x_r] == "c":
-                print("You can't go down as there is a carrot.")
-            elif grid[y_r + 1][x_r] == "O":
-                print("You can't go down as there is a hole. Use 'J' to jump.")
-            else:
-                grid[y_r][x_r] = "-"
-                y_r += 1
-
-        # Works in the same way as when the up or w key is pressed
-        elif event.name == "left" or event.name == "a":
-            if x_r == 0:
-                print("You can't go left. You are at the edge of the board.")
+                grid[y_r + 1][x_r] = "-"
+                picked = True
+            # If there is a carrot to the left of the rabbit
             elif grid[y_r][x_r - 1] == "c":
-                print("You can't go left as there is a carrot.")
-            elif grid[y_r][x_r - 1] == "O":
-                print("You can't go left as there is a hole. Use 'J' to jump.")
-            else:
-                grid[y_r][x_r] = "-"
-                x_r -= 1
-
-        # Works in the same way as when the up or w key is pressed
-        elif event.name == "right" or event.name == "d":
-            if x_r == grid_size - 1:
-                print("You can't go right. You are at the edge of the board.")
+                grid[y_r][x_r - 1] = "-"
+                picked = True
+            # If there is a carrot to the right of the rabbit
             elif grid[y_r][x_r + 1] == "c":
-                print("You can't go right as there is a carrot.")
-            elif grid[y_r][x_r + 1] == "O":
-                print("You can't go right as there is a hole. Use 'J' to jump.")
+                grid[y_r][x_r + 1] = "-"
+                picked = True
+            else:  # If there is no carrot adjacent to the carrot
+                print("Since you are not adjacent to a carrot you can't pick it up")
+        # If the carrot has already been picked and it has to be placed in a hole
+        else:
+            # If the rabbit is at the top of the board
+            if y_r == 0:
+                # If there is a hole adjacent to the rabbit
+                if (
+                    grid[y_r + 1][x_r] == "O"
+                    or grid[y_r][x_r - 1] == "O"
+                    or grid[y_r][x_r + 1] == "O"
+                ):
+                    print("Game Over!!!!")
+                    game_over = True
+                else:
+                    print_nath()
+            # If the rabbit is at the top of the board
+            elif y_r == grid_size - 1:
+                # If there is a hole adjacent to the rabbit
+                if (
+                    grid[y_r - 1][x_r] == "O"
+                    or grid[y_r][x_r - 1] == "O"
+                    or grid[y_r][x_r + 1] == "O"
+                ):
+                    print("Game Over!!!!")
+                    game_over = True
+                else:
+                    print_nath()
+            # If the rabbit is at the left edge of the board
+            elif x_r == 0:
+                # If there is a hole adjacent to the rabbit
+                if (
+                    grid[y_r - 1][x_r] == "O"
+                    or grid[y_r + 1][x_r] == "O"
+                    or grid[y_r][x_r + 1] == "O"
+                ):
+                    print("Game Over!!!!")
+                    game_over = True
+                else:
+                    print_nath()
+            # If the rabbit is at the right edge of the board
+            elif x_r == grid_size - 1:
+                # If there is a hole adjacent to the rabbit
+                if (
+                    grid[y_r - 1][x_r] == "O"
+                    or grid[y_r + 1][x_r] == "O"
+                    or grid[y_r][x_r - 1] == "O"
+                ):
+                    print("Game Over!!!!")
+                    game_over = True
+                else:
+                    print_nath()
+            # If the rabbit is not at the edge of the board
             else:
-                grid[y_r][x_r] = "-"
-                x_r += 1
-
-        # If the j key is pressed the rabbit has to jump
-        elif event.name == "j":
-            # Checks if there is a hole above the rabbit
-            if grid[y_r - 1][x_r] == "O":
-                if (
-                    y_r > 1
-                ):  # Check that if the rabbit jumps it will not go outside the board
-                    grid[y_r][x_r] = "-"
-                    y_r -= 2
-                else:
-                    print_oob()
-            # Checks if there is a hole below the rabbit
-            elif grid[y_r + 1][x_r] == "O":
-                if (
-                    y_r < grid_size - 2
-                ):  # Check that if the rabbit jumps it will not go outside the board
-                    grid[y_r][x_r] = "-"
-                    y_r += 2
-                else:
-                    print_oob()
-            # Checks if there is a hole to the left of the rabbit
-            elif grid[y_r][x_r - 1] == "O":
-                if (
-                    x_r > 1
-                ):  # Check that if the rabbit jumps it will not go outside the board
-                    grid[y_r][x_r] = "-"
-                    x_r -= 2
-                else:
-                    print_oob()
-            # Checks if there is a hole to the right of the rabbit
-            elif grid[y_r][x_r + 1] == "O":
-                if (
-                    x_r < grid_size - 2
-                ):  # Check that if the rabbit jumps it will not go outside the board
-                    grid[y_r][x_r] = "-"
-                    x_r += 2
-                else:
-                    print_oob()
-            # Otherwise it means that there is no hole adjacent to the rabbit
-            else:
-                print("Since there is no hole you cannot jump.")
-
-        # If the p key is pressed it has to either pick up a carrot or place it
-        elif event.name == "p":
-            # If the rabbit has not picked up the carrot
-            if not picked:
-                # If there is a carrot above the rabbit
-                if grid[y_r - 1][x_r] == "c":
-                    grid[y_r - 1][x_r] = "-"
-                    picked = True
-                # If there is a carrot below the rabbit
-                elif grid[y_r + 1][x_r] == "c":
-                    grid[y_r + 1][x_r] = "-"
-                    picked = True
-                # If there is a carrot to the left of the rabbit
-                elif grid[y_r][x_r - 1] == "c":
-                    grid[y_r][x_r - 1] = "-"
-                    picked = True
-                # If there is a carrot to the right of the rabbit
-                elif grid[y_r][x_r + 1] == "c":
-                    grid[y_r][x_r + 1] = "-"
-                    picked = True
-                else:  # If there is no carrot adjacent to the carrot
-                    print("Since you are not adjacent to a carrot you can't pick it up")
-            # If the carrot has already been picked and it has to be placed in a hole
-            else:
-                # If there is a hole adjacent to the rabbit the game ends
+                # If there is a hole adjacent to the rabbit
                 if (
                     grid[y_r - 1][x_r] == "O"
                     or grid[y_r + 1][x_r] == "O"
@@ -293,39 +339,24 @@ while True:
                     or grid[y_r][x_r + 1] == "O"
                 ):
                     print("Game Over!!!!")
-                    exit()
-                elif (
-                    grid[y_r - 1][x_r] == "c"
-                    or grid[y_r + 1][x_r] == "c"
-                    or grid[y_r][x_r - 1] == "c"
-                    or grid[y_r][x_r + 1] == "c"
-                ):
-                    print(
-                        "Since you already have picked up a carrot you can't pick up one more, you have to place the carrot in a hole."
-                    )
-                else:  # If there is no hole adjacent to the rabbit it cannot place the carrot in a hole
-                    print(
-                        "Since you are not adjacent to a hole you can't place it in a hole."
-                    )
+                    game_over = True
+                else:
+                    print_nath()
 
-        # If a carrot has been picked then it become R
-        if picked:
-            grid[y_r][x_r] = "R"
-        else:
-            grid[y_r][x_r] = "r"
+    # If a carrot has been picked then it become R
+    if picked:
+        grid[y_r][x_r] = "R"
+    else:
+        grid[y_r][x_r] = "r"
 
-        # Prints the grid
-        print_grid(grid)
+    # Prints the grid
+    print_grid(grid)
 
-    # Using the keyboard library it waits for the users input
-    keyboard.on_press_key("up", movement)
-    keyboard.on_press_key("w", movement)
-    keyboard.on_press_key("down", movement)
-    keyboard.on_press_key("s", movement)
-    keyboard.on_press_key("left", movement)
-    keyboard.on_press_key("a", movement)
-    keyboard.on_press_key("right", movement)
-    keyboard.on_press_key("d", movement)
-    keyboard.on_press_key("j", movement)
-    keyboard.on_press_key("p", movement)
-    keyboard.wait()  # Waiting for the input
+
+while not game_over:
+    # Reads the keyboard input
+    event = keyboard.read_event()
+
+    # Check if it's a key press event
+    if event.event_type == keyboard.KEY_DOWN:
+        movement(event)
